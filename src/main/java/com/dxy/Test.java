30e7762ffb7bff4c00a9eed8b1d3745e8c4aef40
dingxiya <dingxiya@snsoft.com.cn>
2023-05-25 14:18:11 +0800

    sss

---
+++ src/main/java/com/dxy/Test.java
@@ -0,0 +1,19 @@
+package com.dxy;
+
+/**
+ * <p>标题： </p>
+ * <p>功能： </p>
+ * <p>所属模块： ft-trd</p>
+ * <p>版权： Copyright © 2019 SNSOFT</p>
+ * <p>公司: 北京南北天地科技股份有限公司</p>
+ * <p>创建日期：2023/05/25  14:07</p>
+ * <p>类全名：com.dxy.Test</p>
+ * 作者：dxy
+ * 初审：
+ * 复审：
+ * 监听使用界面:
+ * version 1.0
+ */
+public class Test
+{
+}
